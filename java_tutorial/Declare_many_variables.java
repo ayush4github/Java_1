270ec85c194ed09eeb2156efb8b39fc8f7f5d677
ayush4github <ayushplays712@gmail.com>
2026-01-09 00:37:37 +0530

    Initial commit

---
+++ java_tutorial/Declare_many_variables.java
@@ -0,0 +1,8 @@
+public class Declare_many_variables {
+    public static void main(String[]args){
+        int x = 5;
+        int y = 6;
+        int z = 50;
+        System.out.println(x+y+z);
+    }
+}
---
+++ java_tutorial/One_value_to_multiple_variables.java
@@ -0,0 +1,7 @@
+public class One_value_to_multiple_variables {
+    public static void main(){
+        int x, y, z;
+        x=y=z=50;
+        System.out.println(x+y+z);
+    }
+}
