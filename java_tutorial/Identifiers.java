5af4718a4ceecce4623a20880567aaa3ced545a7
ayush4github <ayushplays712@gmail.com>
2026-01-09 01:45:17 +0530

    initial commit

---
+++ java_tutorial/Identifiers.java
@@ -0,0 +1,7 @@
+public class Identifiers {
+    public static void main(String[]args){
+        int minutesPerHour=60;
+        int m=minutesPerHour;
+        System.out.println("minutesPerHour and m both are identifiers but minutesPerHour is more meaningful than m");
+    }
+}
---
+++ java_tutorial/Multiple_variable_Declaration.java
@@ -0,0 +1,6 @@
+public class Multiple_variable_Declaration {
+    public static void main(String[]args){
+        String firstname = "John", lastname ="Doe";
+        System.out.println(firstname + " " + lastname);
+    }
+}
