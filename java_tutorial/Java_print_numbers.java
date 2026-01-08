6217bc503c4e5eb88006c75a2cd5cb3d0ab2bfcb
ayush4github <ayushplays712@gmail.com>
2026-01-08 15:44:38 +0530

    initial commit

---
+++ java_tutorial/Java_print_numbers.java
@@ -0,0 +1,8 @@
+public class Java_print_numbers {
+    public static void main(String[] args) {
+        System.out.println(100);
+        System.out.println(200);
+        System.out.println(0.50);
+        System.out.println(-200);
+    }
+}
