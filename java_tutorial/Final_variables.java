ddb5a100905d76934e74aa939663fcd147faad4a
ayush4github <ayushplays712@gmail.com>
2026-01-09 16:24:11 +0530

    initial commit

---
+++ java_tutorial/Final_variables.java
@@ -0,0 +1,7 @@
+public class Final_variables {
+    public static void main(String[]args){
+        final int MINUTES_PER_HOUR = 60;
+        final int YEAR_OF_BIRTH = 2022;
+        System.out.println("Values of a final values cannot be changed.");
+    }
+}
