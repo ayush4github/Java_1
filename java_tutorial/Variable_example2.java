92470903f1b06a94f3e82cd080a05b30cfdc23bc
ayush4github <ayushplays712@gmail.com>
2026-01-09 16:44:31 +0530

    initial commit

---
+++ java_tutorial/Variable_example2.java
@@ -0,0 +1,13 @@
+public class Variable_example2{
+    public static void main(String[]args){
+        //Create integer values
+        int length = 4;
+        int breadth = 5;
+        //Calculate area of a rectangle
+        int area = length * breadth;
+        //Print variables
+        System.out.println("Length is: " + length);
+        System.out.println("Breadth is: " + breadth);
+        System.out.println("Area is: " + area);
+    }
+}
