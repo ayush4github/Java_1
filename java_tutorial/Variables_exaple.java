d35588e494ce357e24bc370d9ef013f599bf2df6
ayush4github <ayushplays712@gmail.com>
2026-01-09 16:28:54 +0530

    initial commit

---
+++ java_tutorial/Variables_exaple.java
@@ -0,0 +1,16 @@
+public class Variables_exaple {
+    public static void main(String[]args){
+        // Student data
+        String studentName = "John Doe";
+        int studentID = 15;
+        int studentage = 23;
+        float studentFee = 75.25f;
+        char studentGrade = 'B';
+        // Print variables
+        System.out.println("Student name: " + studentName);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Student age: " + studentage);
+        System.out.println("Student fee is: " + studentFee);
+        System.out.println("Student grade is: " + studentGrade);
+    }
+}
