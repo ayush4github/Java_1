a11d49b6ad08d11a26cfb1f4f35511d3e5c003c5
ayush4github <ayushplays712@gmail.com>
2026-01-13 00:47:46 +0530

    initial commit

---
+++ java_tutorial/Arithmetic_Operators.java
@@ -0,0 +1,16 @@
+public class Arithmetic_Operators {
+    public static void main(String[]args){
+        int x = 10;
+        int y =3;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        System.out.println(x%y);
+        int z = 5;
+        ++z;
+        System.out.println(z);
+        --z;
+        System.out.println(z);
+    }
+}
